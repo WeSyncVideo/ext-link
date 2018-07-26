4f133df497afd50208cdbdf764028750dc5ba02f
Brad <bradpf@outlook.com>
2018-07-26 00:45:35 -0600

    WIP background

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ throw new Error(`
 
   TO IMPORT
 
-  import createStore from 'ext-link/foreground'
+  import getStoreLink from 'ext-link/foreground'
   import createLink from 'ext-link/background'
 
   TO REQUIRE
 
-  const createStore = require('ext-link/foreground')
+  const getStoreLink = require('ext-link/foreground')
   const createLink = require('ext-link/background')
 
   If you are having trouble, please refer to the documentation found at https://github.com/WeSyncVideo/ext-link
--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,37 @@
-function link (api: typeof chrome) {
+import { API, Store, Action, Thunks } from './types'
+import { MESSAGE_TYPE, TARGET_FOREGROUND, isThunkDescriptor } from './common'
+
+// TODO: Consider how modules will impact thunks
+
+function createLink <P, R, S, E, A extends Action> (api: API, store: Store<S, A>, thunks: Thunks<P, R, S, E, A>) {
   const { runtime: { sendMessage, onMessage } } = api
+
+  store.subscribe(() => {
+    const state = store.getState()
+    sendMessage({
+      __type__: MESSAGE_TYPE,
+      __target__: TARGET_FOREGROUND,
+      __state__: state,
+    })
+  })
+
+  onMessage.addListener(message => {
+    if (
+      typeof message === 'object' &&
+      message !== null &&
+      typeof message.__type__ === 'string' &&
+      typeof message.__target__ === 'string' &&
+      typeof message.__action__ === 'object' &&
+      message.__action__ !== null
+    ) {
+      const { action } = message
+      if (isThunkDescriptor(action)) {
+        store.dispatch(thunks[message.thunkName](message.payload))
+      } else {
+        store.dispatch(action)
+      }
+    }
+  })
 }
 
-export default link
+export default createLink
